a61d932edfa4f94719c5f5a15cb5db2af665a779
yamamoto3588 <yamamoto.2zc3588@docomo.ne.jp>
2021-06-06 13:28:26 +0900

    Add body detection feature

--- frontend/routine.js
+++ frontend/routine.js
@@ -1,6 +1,8 @@
 window.addEventListener("DOMContentLoaded",()=>{
     // constants
     const apiServerURL="http://localhost:8001/";
+    const videoWidth=640;
+    const videoHeight=480;
 
     // functions
     function removeStartMenu(){
@@ -27,19 +29,20 @@ window.addEventListener("DOMContentLoaded",()=>{
         const video=Twilio.Video;
         const room=await video.connect(roomInfo.token,{
             audio: true,
-            video: {width: 640}
+            video: {width: videoWidth,height: videoHeight}
         });
         console.log("Successfully joned a Room", room);
 
         // initialize local env
         const localVideoTrack=await video.createLocalVideoTrack();
-        document.getElementById("local-video-container").appendChild(localVideoTrack.attach());
+        const localVideoElm=localVideoTrack.attach();
+        document.getElementById("local-video-container").appendChild(localVideoElm);
+//        localVideoElm.srcObject=null;
 
         // functions for remote video
         function attachToContainer(identity,track){
             const domId="twilio-identity-"+identity
             let container=document.getElementById(domId);
-            console.log(container);
             if(!container){
                 container=document.createElement("div");
                 container.id=domId;
@@ -82,10 +85,63 @@ window.addEventListener("DOMContentLoaded",()=>{
             appendRemoteVideo(participant);
         });
 
-
         document.getElementById("show-room-name").innerHTML="Room name: "+room.name;
         document.getElementById("show-room-token").innerHTML="Your token: <input type='text' value='"+roomInfo.token+"'>";
 
+        // BodyPic
+        console.log("Loading model...")
+        const net=await bodyPix.load({
+            architecture: "ResNet50",
+            outputStride: 16,
+            quantBytes: 2
+        });
+        console.log("Complete",net);
+        // 画像をマスクする
+        function maskCanvas(canvas, img, segmentation) {
+            // マスクを作成
+            const fgColor = { r: 255, g: 0, b: 0, a: 128 };  // 人物は赤、半透明
+            const bgColor = { r: 0, g: 0, b: 0, a: 0 }; // 背景は透明（色は黒だが無関係）
+            const colorMask = bodyPix.toMask(segmentation, fgColor, bgColor);
+
+            // マスクを使って描画
+            const opacity = 1.0;
+            const flipHorizontal = false;
+            const maskBlurAmount = 0;
+            bodyPix.drawMask(
+                canvas, img, colorMask, opacity, maskBlurAmount,
+                flipHorizontal);
+        }
+        
+
+        // MainLoop
+        const localCanvasSrc=document.createElement("canvas");
+        const localCtxSrc=localCanvasSrc.getContext("2d");
+        const localCanvasRes=document.createElement("canvas")
+        const localCtxRes=localCanvasRes.getContext("2d");
+        document.body.appendChild(localCanvasRes);
+        localCanvasSrc.width=videoWidth;
+        localCanvasSrc.height=videoHeight;
+        localCanvasRes.width=videoWidth;
+        localCanvasRes.height=videoHeight;
+        let looping=false;
+        async function loop(){
+            if(!looping){
+                looping=true;
+
+                localCtxSrc.drawImage(localVideoElm,0,0);
+                const segmentation=await net.segmentPersonParts(localCanvasSrc,{
+                    flipHorizontal: false,
+                    internalResolution: "low",
+                    segmentationThreshold: 0.7
+                });
+
+                maskCanvas(localCanvasRes,localCanvasSrc,segmentation);
+
+                looping=false;
+            }
+            requestAnimationFrame(loop);
+        }
+        requestAnimationFrame(loop);
     };
 
     // eventListeners
